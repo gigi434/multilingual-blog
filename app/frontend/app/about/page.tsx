1fa2f1049225facbfc73f650287b762bdbc323ce
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-07-17 20:04:50 +0900

    section2

---
+++ app/frontend/app/about/page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Link from 'next/link'
+export default function Page() {
+  return (
+    <main>
+      aaaa
+      <Link href="/">Home</Link>
+      <Link href="/about/company">About</Link>
+    </main>
+  )
+}
