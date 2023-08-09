3ee220856792fa42cd8672ccb6ca29585a98c419
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-08-09 21:31:30 +0900

    64

--- app/frontend/components/PostBody/index.tsx
+++ app/frontend/components/PostBody/index.tsx
@@ -1,12 +1,33 @@
-import parse from 'html-react-parser'
+import parse, { Element, DOMNode } from 'html-react-parser'
+import Image from 'next/image'
 
 type PostBody = {
   body: string
 }
 
 export function PostBody({ body }: PostBody) {
+  /** 投稿のボディ内に画像がある場合、next/imageコンポーネントとして置き換える */
+  const options = {
+    replace: (domNode: DOMNode) => {
+      if (domNode instanceof Element && domNode.attribs) {
+        if (domNode.name === 'img') {
+          const { src, alt } = domNode.attribs
+          return (
+            <Image
+              src={src}
+              alt={alt}
+              width={1280}
+              height={620}
+              className="my-3 h-auto max-h-[300px] w-full rounded-md object-cover object-center md:max-h-[500px]"
+            />
+          )
+        }
+      }
+    },
+  }
+
   const getParsedHTML = (body: string) => {
-    return parse(body)
+    return parse(body, options)
   }
   return <div className="rich-text">{getParsedHTML(body)}</div>
 }
